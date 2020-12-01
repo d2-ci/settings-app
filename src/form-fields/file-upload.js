b1a747cd39548eec7ecd9e861ef0beec3f7930f7
Philip-Larsen-Donnelly <phil@dhis2.org>
2020-12-01 09:32:09 +0000

    f1f7604 build: fix workflow

---
+++ src/form-fields/file-upload.js
@@ -0,0 +1,219 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import log from 'loglevel';
+import LinearProgress from 'material-ui/LinearProgress';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
+import Checkbox from 'material-ui/Checkbox';
+import applyTranslateContext from '../Translate.HOC';
+import AppTheme from '../theme';
+
+class FileUpload extends React.Component {
+    static propTypes = {
+        name: PropTypes.oneOf(['logo_front', 'logo_banner']).isRequired,
+        label: PropTypes.string.isRequired,
+        value: PropTypes.bool.isRequired,
+        isEnabled: PropTypes.bool.isRequired,
+
+        onFocus: PropTypes.func,
+        onBlur: PropTypes.func,
+        onChange: PropTypes.func,
+    }
+
+    static defaultProps = {
+        onFocus: undefined,
+        onBlur: undefined,
+        onChange: undefined,
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.onClick = this.onClick.bind(this);
+        this.onPreviewClick = this.onPreviewClick.bind(this);
+        this.onToggle = this.onToggle.bind(this);
+        this.onUpload = this.onUpload.bind(this);
+    }
+
+    state = {
+        isEnabled: this.props.isEnabled,
+        uploading: false,
+        progress: undefined,
+        showDialog: false,
+    }
+
+    onClick(e) {
+        if (this.fileInput && !this.state.uploading) {
+            this.fileInput.click(e);
+        } else if (this.state.uploading) {
+            this.xhr.abort();
+            this.setState({ uploading: false, progress: undefined });
+            log.info('File upload cancelled');
+        }
+    }
+
+    onPreviewClick() {
+        this.setState(state => ({ showDialog: !state.showDialog }));
+    }
+
+    onToggle(e) {
+        this.props.onChange({ target: { value: e.target.checked } });
+    }
+
+    onUpload(e) {
+        if (e.target.files.length === 0) {
+            return;
+        }
+
+        this.setState({
+            uploading: true,
+            progress: undefined,
+        });
+
+        const api = this.context.d2.Api.getApi();
+        const xhr = new XMLHttpRequest();
+        xhr.upload.onprogress = (progress) => {
+            if (progress.lengthComputable) {
+                this.setState({ progress: (progress.loaded / progress.total) * 100 });
+            } else {
+                this.setState({ progress: undefined });
+            }
+        };
+        this.xhr = xhr;
+
+        const data = new FormData();
+        data.append('file', e.target.files[0]);
+
+        api.post(['staticContent', this.props.name].join('/'), data).then(() => {
+            log.info('File uploaded successfully');
+            this.props.onChange({ target: { value: true } });
+            this.setState({
+                uploading: false,
+                progress: undefined,
+                isEnabled: true,
+            });
+        }).catch((err) => {
+            log.warn('File upload failed:', err);
+            this.props.onChange({ target: { value: false } });
+            this.setState({
+                uploading: false,
+                progress: undefined,
+                isEnabled: false,
+            });
+        });
+    }
+
+    renderUploading() {
+        const progressStyle = {
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            zIndex: 1,
+        };
+
+        return (
+            <div>
+                <FlatButton label={this.getTranslation('cancel_upload')} onClick={this.onClick} />
+                <div style={progressStyle}>
+                    <LinearProgress
+                        mode={this.state.progress ? 'determinate' : 'indeterminate'}
+                        value={this.state.progress}
+                    />
+                </div>
+            </div>
+        );
+    }
+
+    renderUpload() {
+        const bodyStyle = {
+            backgroundColor: AppTheme.rawTheme.palette.primary1Color,
+            textAlign: 'center',
+            overflow: 'auto',
+            padding: 48,
+        };
+        const dialogImgStyle = {
+            maxWidth: '100%',
+            maxHeight: '70vh',
+        };
+
+        const apiBase = this.context.d2.Api.getApi().baseUrl;
+        const imgUrl = `${[apiBase, 'staticContent', this.props.name].join('/')}?at=${new Date()}`;
+
+        if (this.state.isEnabled) {
+            return (
+                <div>
+                    <FlatButton label={this.getTranslation('replace_image')} secondary onClick={this.onClick} />
+                    <FlatButton label={this.getTranslation('preview_image')} onClick={this.onPreviewClick} />
+                    <Dialog
+                        open={this.state.showDialog}
+                        onRequestClose={this.onPreviewClick}
+                        autoDetectWindowHeight
+                        autoScrollBodyContent
+                        bodyStyle={bodyStyle}
+                    >
+                        <img style={dialogImgStyle} src={imgUrl} alt="preview" />
+                    </Dialog>
+                </div>
+            );
+        }
+
+        return (
+            <FlatButton label={this.getTranslation('upload_image')} primary onClick={this.onClick} />
+        );
+    }
+
+    render() {
+        const { onFocus, onBlur, onChange, ...other } = this.props; // eslint-disable-line
+
+        const containerStyle = {
+            position: 'relative',
+            display: 'block',
+            whiteSpace: 'nowrap',
+        };
+
+        const checkStyle = {
+            display: 'inline-block',
+            whiteSpace: 'nowrap',
+            paddingRight: 8,
+            paddingTop: 8,
+            paddingBottom: 8,
+        };
+
+        const btnStyle = {
+            display: 'inline-block',
+            position: 'absolute',
+            top: 2,
+        };
+
+        const setRef = (ref) => { this.fileInput = ref; };
+
+        return (
+            <div style={containerStyle}>
+                <div style={checkStyle}>
+                    <Checkbox
+                        label={this.props.label}
+                        onCheck={this.onToggle}
+                        disabled={!this.state.isEnabled}
+                        labelStyle={{ color: AppTheme.rawTheme.palette.textColor }}
+                        checked={this.props.value}
+                    />
+                </div>
+                <div style={btnStyle}>
+                    { this.state.uploading ? this.renderUploading() : this.renderUpload() }
+                    <input
+                        type="file"
+                        style={{ visibility: 'hidden', display: 'none' }}
+                        ref={setRef} // eslint-disable-line react/jsx-no-bind
+                        onChange={this.onUpload}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+const FileUploadWithTranslation = applyTranslateContext(
+    FileUpload,
+);
+
+export default FileUploadWithTranslation;
---
+++ src/form-fields/multi-toggle.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Checkbox from 'material-ui/Checkbox';
+
+class MultiToggle extends React.Component {
+    static propTypes = {
+        label: PropTypes.string.isRequired,
+        onChange: PropTypes.func.isRequired,
+        items: PropTypes.arrayOf(PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            value: PropTypes.bool,
+            text: PropTypes.string.isRequired,
+        })),
+        style: PropTypes.object,
+    }
+
+    static defaultProps = {
+        items: [],
+        style: {},
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.onToggle = this.onToggle.bind(this);
+    }
+
+    state = {
+        values: this.props.items.reduce((prev, curr) => {
+            if (curr.value) {
+                prev.push(curr.name);
+            }
+            return prev;
+        }, []),
+    }
+
+    onToggle(value, event, checked) {
+        this.setState((oldState) => {
+            if (checked) {
+                if (oldState.values.indexOf(value) === -1) {
+                    oldState.values.push(value);
+                }
+            } else if (oldState.values.indexOf(value) !== -1) {
+                oldState.values.splice(oldState.values.indexOf(value), 1);
+            }
+            return oldState;
+        }, () => {
+            this.props.onChange({ target: { value: this.state.values } });
+        });
+    }
+
+    render() {
+        const style = Object.assign({}, this.context.muiTheme.forms, this.props.style);
+        return (
+            <div>
+                <div style={{ marginTop: 16, marginBottom: 8 }}>{this.props.label}</div>
+                {this.props.items.map((item) => {
+                    const togglor = this.onToggle.bind(null, item.name); // eslint-disable-line
+                    return (
+                        <Checkbox
+                            key={item.name}
+                            name={item.name}
+                            value="true"
+                            defaultChecked={item.value === true}
+                            label={item.text}
+                            onCheck={togglor} // eslint-disable-line react/jsx-no-bind
+                            style={style}
+                            labelPosition="right"
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
+}
+
+MultiToggle.contextTypes = {
+    muiTheme: PropTypes.object,
+};
+
+export default MultiToggle;
